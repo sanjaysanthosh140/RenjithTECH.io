a8e615bb4f162793b8156e3e25c63386f15e5853
sanjaysanthosh140 <137829617+sanjaysanthosh140@users.noreply.github.com>
2024-05-01 13:19:34 +0530

    Add files via upload

---
+++ product.js
@@ -0,0 +1,85 @@
+
+const product = [
+   {
+       id: 0,
+       image: 'https://i.ytimg.com/vi/kq3zjEN_Fsc/maxresdefault.jpg',
+       title: 'Hand Gripper',
+       price: 120,
+   },
+   {
+       id: 1,
+       image: 'https://tse2.mm.bing.net/th?id=OIP.XwVDLqHW6qYujXK57HSW3QHaHa&pid=Api&P=0&h=220',
+       title: 'Hand Gripper',
+       price: 60,
+   },
+   {
+       id: 2,
+       image: 'https://tse4.mm.bing.net/th?id=OIP.pmIThrJeKyca7IJBSxvGYgHaHa&pid=Api&P=0&h=220',
+       title: 'Hand Gripper',
+       price: 230,
+   },
+   {
+       id: 3,
+       image: 'https://tse2.mm.bing.net/th?id=OIP.O7St_HQqpO7dmJfvtxX28wHaHV&pid=Api&P=0&h=220',
+       title: 'Hand Gripper',
+       price: 100,
+   }
+];
+const categories = [...new Set(product.map((item)=>
+   {return item}))]
+   let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+   var {image, title, price} = item;
+   return(
+       `<div class='box'>
+           <div class='img-box'>
+               <img class='images' src=${image}></img>
+           </div>
+       <div class='bottom'>
+       <p>${title}</p>
+       <h2>$ ${price}.00</h2>`+
+       "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+       `</div>
+       </div>`
+   )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+   cart.push({...categories[a]});
+   displaycart();
+}
+function delElement(a){
+   cart.splice(a, 1);
+   displaycart();
+}
+
+function displaycart(){
+   let j = 0, total=0;
+   document.getElementById("count").innerHTML=cart.length;
+   if(cart.length==0){
+       document.getElementById('cartItem').innerHTML = "Your cart is empty";
+       document.getElementById("total").innerHTML = "$ "+0+".00";
+   }
+   else{
+       document.getElementById("cartItem").innerHTML = cart.map((items)=>
+       {
+           var {image, title, price} = items;
+           total=total+price;
+           document.getElementById("total").innerHTML = "$ "+total+".00";
+           return(
+               `<div class='cart-item'>
+               <div class='row-img'>
+                   <img class='rowimg' src=${image}>
+               </div>
+               <p style='font-size:12px;'>${title}</p>
+               <h2 style='font-size: 15px;'>$ ${price}.00</h2>`+
+               "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+           );
+       }).join('');
+   }
+
+   
+}
